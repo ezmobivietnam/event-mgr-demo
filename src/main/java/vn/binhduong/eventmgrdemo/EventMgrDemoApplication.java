83122adfeb0fe53b1eda6b6e4ceeb4b14acc2cfb
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-11-04 16:27:56 -0400

    Initial - back-end skeleton code.

---
+++ src/main/java/vn/binhduong/eventmgrdemo/EventMgrDemoApplication.java
@@ -0,0 +1,13 @@
+package vn.binhduong.eventmgrdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMgrDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMgrDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/vn/binhduong/eventmgrdemo/web/controllers/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by binhduong on 11/4/2021.
+ */
+package vn.binhduong.eventmgrdemo.web.controllers;
