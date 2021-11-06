df84d0697d700d2c70a44f4612c4154632d132ba
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-11-05 20:06:50 -0400

    [back-end] update sql scripts (h2 and mysql)

--- src/main/resources/db/h2/eventsdb_data.sql
+++ src/main/resources/db/h2/eventsdb_data.sql
@@ -3,10 +3,11 @@ USE `eventsdb`;
 INSERT INTO `eventsdb`.`events`
 (`name`, `description`, `start_date`, `end_date`)
 VALUES
-('event 1', 'This is event 1', CURDATE(), CURDATE() + 1),
-('event 2', 'This is event 2', CURDATE(), CURDATE() + 2),
-('event 3', 'This is event 3', CURDATE(), CURDATE() + 3),
-('event 4', 'This is event 4', CURDATE(), CURDATE() + 4),
-('event 5', 'This is event 5', CURDATE(), CURDATE() + 5),
-('event 6', 'This is event 6', CURDATE(), CURDATE() + 6),
-('event 7', 'This is event 7', CURDATE(), CURDATE() + 7);
+    ('event 1', 'This is event 1', now(), dateadd('DAY', 1, now())),
+    ('event 2', 'This is event 1', now(), dateadd('DAY', 2, now())),
+    ('event 3', 'This is event 1', now(), dateadd('DAY', 3, now())),
+    ('event 4', 'This is event 1', now(), dateadd('DAY', 4, now())),
+    ('event 5', 'This is event 1', now(), dateadd('DAY', 5, now())),
+    ('event 6', 'This is event 1', now(), dateadd('DAY', 6, now())),
+    ('event 7', 'This is event 1', now(), dateadd('DAY', 7, now()))
+;
--- src/main/resources/db/mysql/eventsdb_data.sql
+++ src/main/resources/db/mysql/eventsdb_data.sql
@@ -3,11 +3,12 @@ USE `eventsdb`;
 INSERT INTO `eventsdb`.`events`
 (`name`, `description`, `start_date`, `end_date`)
 VALUES
-('event 1', 'This is event 1', CURDATE(), CURDATE() + 1),
-('event 2', 'This is event 2', CURDATE(), CURDATE() + 2),
-('event 3', 'This is event 3', CURDATE(), CURDATE() + 3),
-('event 4', 'This is event 4', CURDATE(), CURDATE() + 4),
-('event 5', 'This is event 5', CURDATE(), CURDATE() + 5),
-('event 6', 'This is event 6', CURDATE(), CURDATE() + 6),
-('event 7', 'This is event 7', CURDATE(), CURDATE() + 7);
+    ('event 1', 'This is event 1', now(), date_add(now(), INTERVAL 1 DAY)),
+    ('event 2', 'This is event 1', now(), date_add(now(), INTERVAL 2 DAY)),
+    ('event 3', 'This is event 1', now(), date_add(now(), INTERVAL 3 DAY)),
+    ('event 4', 'This is event 1', now(), date_add(now(), INTERVAL 4 DAY)),
+    ('event 5', 'This is event 1', now(), date_add(now(), INTERVAL 5 DAY)),
+    ('event 6', 'This is event 1', now(), date_add(now(), INTERVAL 6 DAY)),
+    ('event 7', 'This is event 1', now(), date_add(now(), INTERVAL 7 DAY))
+;
 
