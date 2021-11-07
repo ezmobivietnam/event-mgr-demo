5a76ff219f2d35e3b901fdfab5b505cad2d4bfcf
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-11-06 21:37:36 -0400

    [frontend] adding 3 components events, events-add and events-list

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+import { EventsAddComponent } from './events/events-add/events-add.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    EventsAddComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
