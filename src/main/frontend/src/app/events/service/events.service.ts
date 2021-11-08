76b8f89651e8cff5902ba7406451a78fb102a55e
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-11-08 14:42:53 -0500

    [frontend] Simply check and display errors receiving from Server at the Service layer instead of the Form layer

--- src/main/frontend/src/app/events/service/events.service.ts
+++ src/main/frontend/src/app/events/service/events.service.ts
@@ -1,6 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, retry, throwError} from "rxjs";
 import {Event} from "../model/event";
 
 @Injectable({
@@ -8,19 +8,46 @@ import {Event} from "../model/event";
 })
 export class EventsService {
 
-    private apiUrl: string;
+    apiUrl: string;
 
-    onTaskAdded = new EventEmitter<Task>();
+    onEventAdded = new EventEmitter<Event>();
 
     constructor(private http: HttpClient) {
         this.apiUrl = '/api/events';
     }
 
     public getEvents(): Observable<Event[]> {
-        return this.http.get<Event[]>('/api/simple/events'); // temporary API endpoint
+        // return this.http.get<Event[]>('/api/simple/events'); // temporary API endpoint
+        return this.http.get<Event[]>('/api/simple/events').pipe(
+            retry(1),
+            catchError(this.handleError)
+        );
     }
 
     public save(event: Event) {
-        return this.http.post<Event>(this.apiUrl, event);
+        return this.http.post<Event>(this.apiUrl, event).pipe(
+            retry(1),
+            catchError(this.handleError)
+        );
     }
+
+    private handleError(error: HttpErrorResponse) {
+        // console.log("error log start");
+        // console.log(error);
+        let errorMsg = error.message;
+        if (error.message) {
+            let details = "";
+            for (const x in error.error) {
+                details += '\n';
+                details += x + ": " + error.error[x];
+            }
+            errorMsg += details;
+        }
+
+        // console.log(errorMsg);
+        // console.log("error log end");
+        window.alert(errorMsg);
+        return throwError('Error occurs from server')
+    }
+
 }
