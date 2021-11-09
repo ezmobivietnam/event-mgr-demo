8dbcae1cab596d54359d2df0acb5b7807433ae9c
ezmobi.vietnam <thanhbinh.duong@gmail.com>
2021-11-09 16:33:50 -0500

    [frontend] fix unit test error: TypeError: Cannot read properties of undefined (reading 'subscribe')

--- src/main/frontend/src/app/events/events-list/events-list.component.spec.ts
+++ src/main/frontend/src/app/events/events-list/events-list.component.spec.ts
@@ -3,17 +3,32 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {EventsListComponent} from './events-list.component';
 import {EventsService} from "../service/events.service";
 import {Event} from "../model/event";
-import {of} from "rxjs";
+import {Observable, of} from "rxjs";
+
+
+class MockEventsService extends EventsService {
+    override getEvents(): Observable<Event[]> {
+        return of([]);
+    }
+
+
+    override save(event: Event): Observable<any> {
+        return of(1);
+    }
+}
 
 describe('EventsListComponent', () => {
     let component: EventsListComponent;
     let fixture: ComponentFixture<EventsListComponent>;
+    let mockHttpClient = jasmine.createSpyObj('HttpClient', ['post', 'get'])
+    let mocService = new MockEventsService(mockHttpClient);
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [EventsListComponent],
             providers: [
-                {provide: EventsService, useValue: jasmine.createSpyObj('EventsService', ['getEvents', 'save'])}
+                // {provide: EventsService, useValue: jasmine.createSpyObj('EventsService', ['getEvents', 'save'])}
+                {provide: EventsService, useValue: mocService}
             ]
         })
             .compileComponents();
